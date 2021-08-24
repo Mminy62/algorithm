af63c27ff5eacb902d76be40513e63b9da86148e
Mminy62 <66752398+Mminy62@users.noreply.github.com>
2021-08-25 03:16:17 +0900

    Create bj_1931.py
    backjoon_1931

---
+++ BOJ/greedy/bj_1931.py
@@ -0,0 +1,26 @@
+'''
+import sys
+
+time = []
+count = 0
+total = 0
+
+n = int(input())
+
+for i in range(n):
+    a,b = map(int, sys.stdin.readline().split())
+    time.append([a,b])
+
+
+time.sort()
+
+for a,b in time:
+    if a < total:
+        if b < total:
+            total = b
+
+    else:# a>= total # cut line
+        total = b
+        count += 1
+
+print(count)
