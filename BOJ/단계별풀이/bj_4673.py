e9876c926a8fa391e19e983d0ff9c3db5bf2cc60
Mminy62 <66752398+Mminy62@users.noreply.github.com>
2022-01-22 21:41:32 +0900

    Update bj_4673.py

--- BOJ/단계별풀이/bj_4673.py
+++ BOJ/단계별풀이/bj_4673.py
@@ -19,4 +19,29 @@ for i in range(1, 10001):
 어차피 1부터 10000까지 모든 수를 d(n)에 넣는 이상
 재귀함수를 통해 나온 값까지도 포함된 경우의 수 이기에
 모든 수의 경우를 다 함수에 넣고 돌리는 선형의 방법이 시간을 확 줄여줬다. 
+그리고 set()자체로 변수 초기화가 가능한지 몰랐었음.
+'''
+'''
+notself = []
+
+def d(n):
+    result = n
+    nlist = list(map(int, str(n)))
+    for temp in nlist:
+        result += temp
+
+    if result > 10000:
+        return 0
+    else:
+        d(result)
+        self.append(result)
+
+for i in range(1, 10001):
+    d(i)
+
+self = set(notself)
+
+for i in range(1, 10001):
+    if i not in notself:
+        print(i)
 '''
