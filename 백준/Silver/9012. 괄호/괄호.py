a3f95cc43e5f95c4685913b0be7830b303fce866
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2022-12-18 20:52:24 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -9,17 +9,15 @@ for _ in range(n):
 for line in tests:
     stack = []
     for w in line:
-        top = len(stack) - 1 #top is index of stack top/end
         if w == '(' or not bool(stack):
             stack.append(w)
         else:# w is ')' and not empty stack
-            if top >= 0 and stack[top] == '(':
+            top = stack[-1]
+            if top == '(':
                 stack.pop()
             else:
                 stack.append(w)
-                
-            
-        
+  
     if len(stack) > 0:
         result.append("NO")
     else:
