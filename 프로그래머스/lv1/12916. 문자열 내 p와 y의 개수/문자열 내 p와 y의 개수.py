28bbdc42f8430621eba96b9f4285603d4a4fc51b
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-01-02 18:29:38 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
@@ -1,10 +1,11 @@
 def solution(s):
-    s = list(s.lower())
+    return s.lower().count('p') == s.lower().count('y')
+#     s = list(s.lower())
     
-    pcnt = len(list(filter(lambda x : x == "p", s)))
-    ycnt = len(list(filter(lambda x : x == "y", s)))
+#     pcnt = len(list(filter(lambda x : x == "p", s)))
+#     ycnt = len(list(filter(lambda x : x == "y", s)))
     
-    if pcnt == ycnt:
-        return True
-    else:
-        return False
+#     if pcnt == ycnt:
+#         return True
+#     else:
+#         return False
