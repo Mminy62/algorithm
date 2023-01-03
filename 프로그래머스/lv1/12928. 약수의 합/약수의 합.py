4932cd79cbbaeabb5eff28b5741cec677055403d
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-01-03 21:25:50 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -1,11 +1,3 @@
 def solution(n):
-    answer = 0
-    for i in range(1, int(n**0.5) + 1):
-        if n <= 1:
-            answer = n
-        elif n % i == 0:
-            answer += i
-            if n // i != i:
-                answer += (n//i)
         
-    return answer
+    return n + sum([i for i in range(1, (n // 2) + 1) if n % i == 0])
