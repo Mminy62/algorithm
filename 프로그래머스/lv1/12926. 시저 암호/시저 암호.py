50f8aaf60f4aa2e828df17102a22cff3e2e2793b
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-01-03 20:45:06 +0900

    [level 1] Title: 시저 암호, Time: 2.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -1,15 +1,29 @@
 def solution(s, n):
-    answer = []
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i]=chr((ord(s[i])-ord('A')+ n)%26+ord('A'))
+        elif s[i].islower():
+            s[i]=chr((ord(s[i])-ord('a')+ n)%26+ord('a'))
+
+    return "".join(s)
+    # 주어진 문장을 암호화하여 반환하세요.
+
+
+# 실행을 위한 테스트코드입니다.
+#print('s는 "a B z", n은 4인 경우: ' + caesar("a B z", 4))
+# def solution(s, n):
+#     answer = []
     
-    for c in list(s):
-        if c.isalpha():
-            temp = ord(c) + n
+#     for c in list(s):
+#         if c.isalpha():
+#             temp = ord(c) + n
     
-            if c.islower() and temp > 122 or c.isupper() and temp > 90:
-                temp -= 26
+#             if c.islower() and temp > 122 or c.isupper() and temp > 90:
+#                 temp -= 26
             
-            answer.append(chr(temp))
-        else:
-            answer.append(c)
+#             answer.append(chr(temp))
+#         else:
+#             answer.append(c)
                 
-    return ''.join(answer)
+#     return ''.join(answer)
