dd05c6061ab6d25d0f2f076e7222451d1321e60b
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-01-04 16:45:09 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,14 +1,12 @@
 def solution(num):
-    answer = 0
     cnt = 0
 
     while(cnt < 501):
         if num == 1:
             return cnt
-        elif num % 2 == 0:
-            num = num // 2
-        else:
-            num = (num * 3) + 1
+        
+        num = num / 2 if not num % 2  else (num * 3 + 1)
+        
         cnt += 1
     
     return -1
