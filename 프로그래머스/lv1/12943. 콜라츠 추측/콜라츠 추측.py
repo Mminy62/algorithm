7f69eb0c9c398e1285488c8a448af3e6e3b9e5fb
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-01-04 16:41:27 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,17 @@
+def solution(num):
+    answer = 0
+    cnt = 0
+    
+    if num == 1:
+        return 0
+    
+    while(cnt < 501):
+        if num == 1:
+            return cnt
+        elif num % 2 == 0:
+            num = num // 2
+        else:
+            num = (num * 3) + 1
+        cnt += 1
+    
+    return -1
