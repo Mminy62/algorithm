ec2d4c77245ece3c2b85a66c19bc2f5a6dc8c96d
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-01-04 16:28:59 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,15 +1,16 @@
-def gcd(a, b): #a 가 더 큰 수로 가정
-    if a < b: b, a = a, b
-    if b == 0: 
-        return a
-    else:
-        return gcd(b, a % b)
+# def gcd(a, b): #a 가 더 큰 수로 가정
+#     if a < b: b, a = a, b
+#     if b == 0: 
+#         return a
+#     else:
+#         return gcd(b, a % b)
     
-def lcm(m, g):
-    return m // g
-    
+# def lcm(m, g):
+#     return m // g
+import math
+
 def solution(n, m):
-    g = gcd(n, m)
-    l = lcm(n*m, g)
+    g = math.gcd(n, m)
+    l = n*m//g
     
     return [g, l]
