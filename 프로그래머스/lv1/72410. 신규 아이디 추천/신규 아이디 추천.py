e5972878a3cc1fbead024f33f53d7788af08d9bc
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-01-06 00:00:51 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,18 +1,14 @@
 import re
 def solution(new_id):
-    #1
     new_id = new_id.lower()
-    #2
     new_id = re.sub(r"[~!@#$%^&*()=+\[\{\]\}:?,<>/]", "", new_id)
-    #3
     while(".." in new_id):
         new_id = new_id.replace("..", ".")
-    #4
     new_id = new_id.strip(".")
-    #5
+    
     if not new_id:
-        new_id = "aaa" 
-    #6
+        new_id = "aaa"
+        
     elif len(new_id) > 15:
         new_id = new_id[:15]
         new_id = new_id.rstrip(".")
