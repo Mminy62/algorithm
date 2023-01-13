1f6d03038969ec986691db7faaf2d9d48f03e55b
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-01-14 02:11:42 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,21 +1,13 @@
 def solution(k, score):
+
+    q = []
+
     answer = []
-    honor = []
-    
-    for i, v in enumerate(score):
-        if k >= len(score):
-            honor.append(v)
-            honor.sort()
-            
-        else:
-            if len(honor) < k:
-                honor.append(v)
-                honor.sort()
-            else:
-                if v > honor[0]:
-                    honor[0] = v
-                    honor.sort()
-            
-        answer.append(honor[0])     
-    
+    for s in score:
+
+        q.append(s)
+        if (len(q) > k):
+            q.remove(min(q))
+        answer.append(min(q))
+
     return answer
