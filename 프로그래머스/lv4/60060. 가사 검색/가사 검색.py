65f304b572069de983e3505f6b076951a7ca9f7b
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-02-13 12:38:40 +0900

    [level 4] Title: 가사 검색, Time: 7.39 ms, Memory: 14.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/60060. 가사 검색/가사 검색.py
@@ -0,0 +1,47 @@
+from bisect import bisect_left, bisect_right
+
+
+def solution(words, queries):
+    answer = []
+    '''
+    풀이참조
+    접미사면 right를 찾을때 z로 대치
+    접두사면 시작부터 전부 다 찾기
+    '''
+
+    #길이를 기준으로 해본 다음에 효율성 낮으면 알파벳 기준으로 한번 더 이진탐색
+
+    array = [[] for _ in range(10001)]
+    reversed_array = [[] for _ in range(10001)]
+
+    for word in words:
+        array[len(word)].append(word)
+        reversed_array[len(word)].append(word[::-1])
+
+    for i in range(10001):
+        array[i].sort()
+        reversed_array[i].sort()
+
+    for query in queries:
+        count = 0
+        query_length = len(query)
+        array_length = len(array[query_length])
+        left = bisect_left(array[len(query)], query)
+
+        if query[-1] == "?":#접미사
+            left = bisect_left(array[len(query)], query.replace("?", "a"))
+            right = bisect_right(array[len(query)], query.replace("?", "z"))
+
+
+        else:#접두사인 경우 길이별로 순차 탐색
+            left = bisect_left(reversed_array[len(query)], query[::-1].replace("?", "a"))
+            right = bisect_right(reversed_array[len(query)], query[::-1].replace("?", "z"))
+
+
+        if left == right:
+            answer.append(0)
+        else:
+            count = right - left
+            answer.append(count)
+
+    return answer
