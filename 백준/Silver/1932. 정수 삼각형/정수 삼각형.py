0f9e7d41e9f93e39c606dcc37766b50476e01fa0
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-02-16 01:47:09 +0900

    [Silver I] Title: 정수 삼각형, Time: 168 ms, Memory: 116020 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,25 @@
+n = int(input())
+m = n
+array = []
+
+for i in range(n):
+    array.append(list(map(int, input().split())))
+
+
+def dp(array):
+
+    if n == 1:
+        return array[0]
+
+    for i in range(n - 2, -1, -1):  # 3 2 1 0 # row
+        for j in range(i, -1, -1):  # 4 3 2 1 0 -> 3 2 1 0 -> 2 1 0 # col
+
+            left_down = array[i][j] + array[i + 1][j]
+            right_down = array[i][j] + array[i + 1][j + 1]
+
+            array[i][j] = max(left_down, right_down)
+
+
+    return array[0]
+
+print(*dp(array))
