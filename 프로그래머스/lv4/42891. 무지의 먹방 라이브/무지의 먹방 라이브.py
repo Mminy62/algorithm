30307cc1490531e9b733c9d6a85e1ad342593a73
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-03-06 16:00:55 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 154.60 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/lv4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
+++ 프로그래머스/lv4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -1,23 +1,27 @@
 import heapq
+
 def solution(food_times, k):
+    answer = 0
     
     if sum(food_times) <= k:
         return -1
     
     q = []
     for i in range(len(food_times)):
-        # (음식 시간, 음식 번호) 형태로 우선순위 큐에 삽입
-        heapq.heappush(q, (food_times[i], i + 1))  
+        heapq.heappush(q, (food_times[i], i + 1))
     
-    sumvalue = 0
-    previous = 0
+    #이전에 더한 값
+    sum_value = 0
+    previous_value = 0
     length = len(food_times)
-    
-    while sumvalue + ((q[0][0] - previous) * length) <= k:
-        now = heapq.heappop(q)[0]
-        sumvalue += (now - previous) * length
+    count = 0
+
+    while sum_value + (q[0][0] - previous_value) * length <= k:
+        now, i = heapq.heappop(q)
+        sum_value += (now - previous_value) * length
+        previous_value = now
         length -= 1
-        previous = now
-        
-    result = sorted(q, key = lambda x:x[1])
-    return result[(k - sumvalue) % length][1]
+
+    q = sorted(q, key = lambda x: x[1])
+
+    return q[(k-sum_value) % length][1]
