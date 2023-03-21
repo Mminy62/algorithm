b429c5ddf1d1bae65db4f75d200e8ec3aa111266
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-03-21 14:49:41 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.52 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,9 +1,22 @@
-from math import comb
-def solution(n):#~2시까지
-    answer = 1
-    for i in range(1, n//2 + 1): # i 는 2의 개수
-        cnt2 = n - i * 2
-        numbers = i + cnt2
-        answer += comb(numbers, i)
+'''
+n = 4 일 때,
+마지막에 1칸을 뛸 경우 그 앞칸(n = 3)의 방법에서 +1만 하면 된다. 따라서 dp[i-1]
+마지막에 2칸을 뛸 경우 그 앞앞칸(n = 2)의 방법에서 +2만 하면 된다. 따라서 dp[i-2]
+'''
+def solution(n):
+    dp = [0] * (n + 1)
+    dp[0], dp[1] = 1, 1
     
-    return answer % 1234567
+    for i in range(2, n + 1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+        
+    return dp[n]
+# from math import comb
+# def solution(n):#~2시까지
+#     answer = 1
+#     for i in range(1, n//2 + 1): # i 는 2의 개수
+#         cnt2 = n - i * 2
+#         numbers = i + cnt2
+#         answer += comb(numbers, i)
+    
+#     return answer % 1234567
