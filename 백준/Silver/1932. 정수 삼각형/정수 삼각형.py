95504ccaecca1d28f8ec9be0637482bdc8d4c2ec
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-03-28 15:27:32 +0900

    [Silver I] Title: 정수 삼각형, Time: 164 ms, Memory: 117152 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,25 +1,19 @@
 n = int(input())
-m = n
-array = []
-
+graph = []
+dp = []
 for i in range(n):
-    array.append(list(map(int, input().split())))
+    graph.append(list(map(int, input().split())))
+    dp.append([0] * (i + 1))
 
+dp[0] = graph[0]
 
-def dp(array):
+for i in range(1, n):
+    for j in range(len(graph[i])):
+        if j == 0:
+            dp[i][j] = graph[i][j] + dp[i-1][j]
+        elif j == len(graph[i])-1:
+            dp[i][j] = graph[i][j] + dp[i-1][j-1]
+        else:
+            dp[i][j] = graph[i][j] + max(dp[i-1][j-1], dp[i-1][j])
 
-    if n == 1:
-        return array[0]
-
-    for i in range(n - 2, -1, -1):  # 3 2 1 0 # row
-        for j in range(i, -1, -1):  # 4 3 2 1 0 -> 3 2 1 0 -> 2 1 0 # col
-
-            left_down = array[i][j] + array[i + 1][j]
-            right_down = array[i][j] + array[i + 1][j + 1]
-
-            array[i][j] = max(left_down, right_down)
-
-
-    return array[0]
-
-print(*dp(array))
+print(max(dp[n-1]))
