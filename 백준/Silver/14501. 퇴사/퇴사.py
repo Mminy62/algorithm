a0b6a990f787bb23bc73ef597075aff7a7bf7df7
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-03-28 17:19:25 +0900

    [Silver III] Title: 퇴사, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,21 +1,26 @@
 n = int(input())
+days = [0] * (n + 1)
+prices = [0] * (n + 1)
+for i in range(1, n + 1):
+    d, p = map(int, input().split())
+    days[i], prices[i] = d, p
 
-dp = [0] * (n+1)
-array = [[0]]
-max_value = 0
+dp = [0] * (n + 1)
+if days[n] == 1:
+    dp[n] = prices[n]
 
-for i in range(n):
-    array.append(list(map(int, input().split())))
-
-for i in range(n, 0, -1):
-    time = i + array[i][0]
-    if time <= n + 1:#현재 날짜가 포함되는 경우
-        dp[i] += array[i][1]
-        if time <= n:# 현재 날짜의 상담 간격만큼 더해주는 경우
-            dp[i] += dp[time]
-        dp[i] = max(dp[i], max_value)
-        max_value = dp[i]
+for i in range(n - 1, 0, -1):
+    # i -> today
+    next_day = i + days[i]
+    if next_day > n + 1:
+        # n이 10인경우, 8일차 + 3일 - 1은 10이므로 8일차의 값을 더해도 된다.
+        dp[i] = dp[i+1]
+        continue
+    if next_day == n + 1:
+        # 같은 날엔 다음 next day 값을 더하지 않고 해당 날의 값만 비교한다.
+        next_day = prices[i]
     else:
-        dp[i] = max_value
+        next_day = prices[i] + dp[next_day]
+    dp[i] = max(dp[i+1], next_day)
 
-print(max_value)
+print(dp[1])
