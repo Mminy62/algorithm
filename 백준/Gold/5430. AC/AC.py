69faaa6f61ef3031c1b754e0fc5a372b380cc567
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-06-28 16:56:29 +0900

    [Gold V] Title: AC, Time: 3700 ms, Memory: 134100 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+
+tc = int(input())
+result = []
+for _ in range(tc):
+    cmds = input().rstrip()
+    length = int(input())
+    stack = input().rstrip()[1:-1].split(',')
+    flag = True
+
+    if cmds.count('D') > length:
+        result.append('error')
+
+    else:
+        if 'RR' in cmds:
+            cmds = cmds.replace('RR', '')
+
+        for i in range(len(cmds)):
+
+            if cmds[i] == 'R':
+                flag = not flag
+
+            else:#'D'
+                if flag:
+                    del stack[0]
+                else:
+                    del stack[-1]
+        
+        if not stack:
+            result.append(stack)
+        elif flag:
+            result.append('[' + ','.join(stack) + ']')
+        else:
+            result.append('[' + ','.join(stack[::-1]) + ']')
+            
+
+
+for k in result:
+    print(k)
