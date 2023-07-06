ae5222c81cca25f721848f0e03a1aadb155d8b54
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-07-06 15:17:26 +0900

    [Silver IV] Title: 듣보잡, Time: 172 ms, Memory: 123528 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -3,42 +3,15 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-see = []
-listen = []
+s1 = set()
+s2 = set()
+
 for _ in range(n):
-    see.append(input().rstrip())
-
+    s1.add(input())
 for _ in range(m):
-    listen.append(input().rstrip())
+    s2.add(input())
 
-see.sort()
-listen.sort()
-result = []
-
-def binary_search(array, item, length):
-    start = 0
-    end = length - 1
-
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] == item:
-            return True
-        elif array[mid] < item:
-            start = mid + 1
-        else:
-            end = mid - 1
-
-    return False
-
-if n < m:
-    # listen 기준 탐색
-    for i in range(n):
-        if binary_search(listen, see[i], m):
-            result.append(see[i])
-else:
-    for i in range(m):
-        if binary_search(see, listen[i], n):
-            result.append(listen[i])
+result = list(sorted(s1 & s2))
 
 print(len(result))
-print('\n'.join(result))
+print(''.join(result))
