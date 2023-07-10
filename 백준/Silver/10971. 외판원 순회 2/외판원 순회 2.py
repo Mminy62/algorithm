eab8fd0a5f852e3053221094584b32239acd5656
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-07-10 19:15:18 +0900

    [Silver II] Title: 외판원 순회 2, Time: 1716 ms, Memory: 118164 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -4,30 +4,30 @@ graph = []
 
 for _ in range(n):
     graph.append(list(map(int, input().split())))
-
-dist = []
+    
 node = []
+min_dist = 1e9
 
 def dfs():
-    
+    global min_dist
+
     if len(node) > 1 and graph[node[-2]][node[-1]] == 0:
         return
 
-    if len(node) == n:
-        if graph[node[-1]][node[0]] == 0:
-            return
+    if len(node) == n and graph[node[-1]][node[0]] != 0:
         temp = 0
         for i in range(n-1):
             temp += graph[node[i]][node[i+1]]
         temp += graph[node[-1]][node[0]]
-        dist.append(temp)
+        min_dist = min(min_dist, temp)
         return
 
     for i in range(n):
-        if i not in node: # and graph[node[-1]][j]
+        if i not in node:# and graph[node[-1]][j]
             node.append(i)
             dfs()
             node.pop()
 
+
 dfs()
-print(min(dist))
+print(min_dist)
