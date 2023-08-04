30a127afa3c3f29d69957d2498eca6708c2f1543
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-08-04 11:34:29 +0900

    [Gold V] Title: 토마토, Time: 780 ms, Memory: 219016 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,65 +1,47 @@
-'''
-- 익은 토마토의 자리 위치, 토마토 위치(-1 출력을 위해) 자리 받아놓기
-- 익은 토마토부터 BFS 시작 전염된 것은 큐에 넣기
-- day를 포함해서 큐에 넣기
-- day가 빠른 것부터 꺼내쓰는 우선순위큐
-- 마지막 day 출력
-'''
-import sys
+from sys import stdin
 from collections import deque
-input = sys.stdin.readline
 
-M, N, H = map(int, input().split())
-box = [[] for _ in range(H)]
-pos = []
-q = deque([])
-result = -1
+M,N,H = map(int, stdin.readline().split())
+li = [[[0]*M for i in range(N)]for j in range(H)]
+count_one = 0
+count_minus =0
+max_num = 1
+queue = deque()
 
-# input
 for i in range(H):
     for j in range(N):
-        line = list(map(int, input().split()))
-        box[i].append(line)
-        done = list(filter(lambda x: line[x] == 1, range(M)))
-        temp = list(filter(lambda x: line[x] == 1 or line[x] == 0, range(M)))
-        for k in temp:
-            pos.append((i, j, k))
-        for k in done:
-            q.append((0, i, j, k))
+        tmp = list(map(int, stdin.readline().split()))
+        for k in range(M):
+            li[i][j][k] = tmp[k]
+            if tmp[k] == 1:
+                queue.append((i,j,k))
+                count_one +=1
+            elif tmp[k] == -1:
+                count_minus +=1
 
-def check():
-    max_day = 0
-    for (z, x, y) in pos:
-        if box[z][x][y] == 0:
-            return False
-        max_day = max(max_day, box[z][x][y])
-    return max_day
+ans =M*N*H - count_minus
 
-dx = [-1, 1, 0, 0, 0, 0]
-dy = [0, 0, -1, 1, 0, 0]
-dz = [0, 0, 0, 0, -1, 1]
-
-if check():
-    result = 0
+dx, dy, dz = [1, -1, 0, 0, 0, 0], [0, 0, 1, -1, 0, 0], [0, 0, 0, 0, 1, -1]
+        
+if ans - count_one== 0 :
+    print(0)
 else:
-    while q:
-        day, z, x, y = q.popleft()
-
-        c = check()
-        if c:
-            result = c
-            break
+    while queue:
+        z, y, x = queue.popleft()
 
         for i in range(6):
-            nz, nx, ny = z + dz[i], x + dx[i], y + dy[i]
-            if nx < 0 or nx >= N or ny < 0 or ny >= M or nz < 0 or nz >= H:
-                continue
-            if box[nz][nx][ny] == -1:
-                continue
-            # 해당 위치
-            if box[nz][nx][ny] == 0:
-                box[nz][nx][ny] = day + 1
-                q.append((day+1, nz, nx, ny))
+            nx = x + dx[i]
+            ny = y + dy[i]
+            nz = z + dz[i]
 
-
-print(result)
+            if 0<= nx < M and 0<=ny < N and 0 <= nz < H:
+                if li[nz][ny][nx] == 0:
+                    li[nz][ny][nx] = li[z][y][x] +1
+                    max_num = max(li[nz][ny][nx],max_num)
+                    count_one +=1
+                    queue.append((nz,ny,nx))
+        
+    if count_one == ans:
+        print(max_num -1)
+    else:
+        print(-1)
