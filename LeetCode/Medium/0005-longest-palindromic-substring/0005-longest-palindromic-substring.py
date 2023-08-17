dffb3520e1b56b48b2412976ae49ceb084eb5cd7
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-08-17 14:49:32 +0900

    Time: 382 ms (91.73%), Space: 16.2 MB (99.59%) - LeetHub

--- LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,11 +1,25 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # 가장 긴 팰린드롬 문자열
-        result = ''
-        for l in range(len(s)):
-            for r in range(l+1, len(s)+1):
-                if s[l:r] == s[l:r][::-1] and len(result) < r-l:
-                    result = s[l:r]
+#         result = ''
+#         for l in range(len(s)):
+#             for r in range(l+1, len(s)+1):
+#                 if s[l:r] == s[l:r][::-1] and len(result) < r-l:
+#                     result = s[l:r]
 
-        return result
-        
+#         return result
+        def expand_around_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+        longest = ""
+        for i in range(len(s)):
+            odd_palindrome = expand_around_center(i, i)
+            even_palindrome = expand_around_center(i, i + 1)
+            
+            if len(odd_palindrome) > len(longest):
+                longest = odd_palindrome
+            if len(even_palindrome) > len(longest):
+                longest = even_palindrome
+                
+        return longest
