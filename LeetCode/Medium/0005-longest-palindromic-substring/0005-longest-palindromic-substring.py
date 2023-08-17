442742067121d2cb3537a5a858d38e6805dad727
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-08-17 10:21:45 +0900

    Time: 8847 ms (5.00%), Space: 16.5 MB (28.25%) - LeetHub

--- LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -6,7 +6,6 @@ class Solution:
             for r in range(l+1, len(s)+1):
                 if s[l:r] == s[l:r][::-1] and len(result) < r-l:
                     result = s[l:r]
-        if not result and len(s) == 1:
-            result = s
+
         return result
         
