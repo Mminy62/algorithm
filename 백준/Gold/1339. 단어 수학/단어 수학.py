09077dd6cc3c428cbb697a983f06116815025102
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-08-20 03:59:10 +0900

    [Gold IV] Title: 단어 수학, Time: 144 ms, Memory: 114436 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.py
+++ 백준/Gold/1339. 단어 수학/단어 수학.py
@@ -1,17 +1,15 @@
+from collections import defaultdict
 n = int(input())
 array = []
-dicts = {}
+dicts = defaultdict(int)
 
 for _ in range(n):
     temp = input()
     array.append(temp)
     for i in range(len(temp)):
-        if temp[i] not in dicts:
-            dicts[temp[i]] = 10 ** (len(temp) - i - 1)
-        else:
-            dicts[temp[i]] += 10 ** (len(temp) - i - 1)
+        dicts[temp[i]] += 10 ** (len(temp) - i - 1)
 
-dicts = sorted(dicts.items(), key = lambda item: item[1], reverse = True)
+dicts = sorted(dicts.items(), key=lambda item: item[1], reverse=True)
 
 v = 9
 new_dicts = {}
