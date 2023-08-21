00bb4d63d9fa2f60917e0fea46c40cc729c37dc7
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-08-21 22:45:53 +0900

    Time: 98 ms (84.71%), Space: 19.5 MB (84.89%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.py
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.py
@@ -1,19 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dicts = {}
+        dicts = collections.defaultdict(list)
+        
         for i in range(len(strs)):
-            temp = tuple(sorted(strs[i]))
-            if temp not in dicts:
-                dicts[temp] = [i]
-            else:
-                dicts[temp].append(i)
-                
-        dvs = list(dicts.values())
-        result = []
-        for data in dvs:
-            tmp = []
-            for idx in data:
-                tmp.append(strs[idx])
-            result.append(tmp)
-            
-        return result
+            dicts[''.join(sorted(strs[i]))].append(strs[i])
+        
+        return list(dicts.values())
