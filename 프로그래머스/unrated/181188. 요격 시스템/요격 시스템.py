26cea91c558b0bce3258819f9fc54c32c868d749
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-10-05 00:33:31 +0900

    [unrated] Title: 요격 시스템, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
@@ -1,14 +1,14 @@
 def solution(targets):
     targets.sort(key=lambda x: (x[0], x[1]))
 
-    cnt = 1
-    end = targets[0][1]
-    for i in range(1, len(targets)):
+    cnt = 0
+    end = -1
+    for i in range(len(targets)):
         s, e = targets[i][0], targets[i][1]
         if end <= s:
             cnt += 1
             end = e
         else:# 같은 라인
             end = min(end, e)
-    print(cnt)
+
     return cnt
