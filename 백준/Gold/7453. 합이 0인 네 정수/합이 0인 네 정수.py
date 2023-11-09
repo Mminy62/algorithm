13ae62d1171ada72256fec392939b2c38d275130
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2023-11-10 00:19:28 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 6856 ms, Memory: 641084 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
@@ -1,32 +1,50 @@
-n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-ab, cd = [], []
-for i in range(n):
-    for j in range(n):
-        ab.append(arr[i][0] + arr[j][1])
-        cd.append(arr[i][2] + arr[j][3])
+N = int(input())
 
-ab.sort()
-cd.sort()
-# print(ab, cd)
-i, j = 0, len(cd) - 1  # i는 ab의 시작점, j는 cd의 끝점(투포인터)
-result = 0
-while i < len(ab) and j >= 0:
-    if ab[i] + cd[j] == 0:  # 합이 0이 되는 경우
-        nexti, nextj = i + 1, j - 1
-        # ab가 같은게 여러개인경우
-        while nexti < len(ab) and ab[i] == ab[nexti]:
+A, B, C, D = [], [], [], []
+for _ in range(N):
+    temp = list(map(int, input().split()))
+    A.append(temp[0])
+    B.append(temp[1])
+    C.append(temp[2])
+    D.append(temp[3])
+
+arrAB = []
+arrCD = []
+
+for i in range(N):
+    for j in range(N):
+        arrAB.append(A[i] + B[j])
+
+for i in range(N):
+    for j in range(N):
+        arrCD.append(C[i] + D[j])
+
+arrAB.sort()
+arrCD.sort()
+
+
+i = 0
+j = len(arrCD) - 1
+cnt = 0
+
+while i < len(arrAB) and j >= 0:
+    temp = arrAB[i] + arrCD[j]
+    if temp == 0:
+        nexti = i + 1
+        nextj = j - 1
+        while nexti < len(arrAB) and arrAB[nexti] == arrAB[i]:
             nexti += 1
-        # cd가 같은게 여러개인경우
-        while nextj >= 0 and cd[j] == cd[nextj]:
+        while nextj >= 0 and arrCD[nextj] == arrCD[j]:
             nextj -= 1
 
-        result += (nexti - i) * (j - nextj)  # 이야... 지리네
-        i, j = nexti, nextj
+        cnt += abs(nexti - i) * abs(nextj - j)
+        i = nexti
+        j = nextj
 
-    elif ab[i] + cd[j] > 0:  # cd가 ab보다 더 절댓값이 큰경우
+    elif temp > 0:
         j -= 1
-    else:  # ab가 cd보다 더 절댓값이 큰 경우
+    else:
         i += 1
 
-print(result)
+print(cnt)
+
