858bc20182fee3ac9463c5b829f86a0d19a7a839
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-03-11 10:30:26 +0900

    [Gold V] Title: 빗물, Time: 132 ms, Memory: 111744 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,34 +1,25 @@
-
-n, m = map(int, input().split())
+H, W = map(int, input().split())
 walls = list(map(int, input().split()))
+array = [[0] * W for _ in range(H)]
 
-# input 값 정리
-# h, w 만큼 2차원 배열 만들기
-graph = [[0] * m for _ in range(n)]
-
-for j in range(len(walls)):
-    for i in range(n-1, n-1-walls[j], -1):
-        graph[i][j] = 1
-
-starts = []
-for i in range(n):
-    for j in range(1, m):
-        if graph[i][j] == 0 and graph[i][j-1] == 1:
-            starts.append((i, j))
+for i in range(W):
+    if walls[i]:#0이상이면
+        for r in range(0, walls[i]):
+            array[r][i] = 1
 
 result = 0
-for x, y in starts:
-    cnt = 1
-
-    while y + 1 < m:
-        y += 1
-        if graph[x][y] == 0:
-            cnt += 1
-        if graph[x][y] == 1:
-            result += cnt
-            break
-
+for c in range(W):
+    for r in range(H):
+        count = 0
+        if array[r][c] and c + 1 < W:
+            nx = r
+            ny = c + 1
+            while array[nx][ny] != 1:# 1이면 멈추고 count 더하기
+                count += 1
+                ny += 1
+                if ny > W - 1:
+                    count = 0
+                    break
+        result += count
 
 print(result)
-
-
