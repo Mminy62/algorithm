4dd2bfd0becb7d9a242b49bf3a87b233cd80e515
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-03-20 10:43:38 +0900

    [Silver II] Title: 병사 배치하기, Time: 140 ms, Memory: 110592 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -1,13 +1,11 @@
 n = int(input())
-cnt = 0
-
 soldiers = list(map(int, input().split()))
-soldiers.reverse()
-dp = [1] * n #부분 수열의 명 수 표시 #해당 인덱스까지의 연속된 숫자
 
+soldiers.reverse()
+dp = [1] * n
 for i in range(1, n):
     for j in range(0, i):
-        if soldiers[j] < soldiers[i]:
+        if soldiers[i] > soldiers[j]:
             dp[i] = max(dp[i], dp[j] + 1)
 
 print(n - max(dp))
