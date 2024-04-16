67f3cc19fdc3ce3a4aae3fbc924b03394054adc7
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-04-16 14:09:46 +0900

    [level 2] Title: 귤 고르기, Time: 23.62 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,16 +1,10 @@
 from bisect import bisect_left
+from collections import Counter
 
 def solution(k, tangerine):
     answer = 0
-    size_dict = {}
-    for i in range(len(tangerine)):
-        item = tangerine[i]
-        if item not in size_dict.keys():
-            size_dict[item] = 1
-        else:
-            size_dict[item] += 1
-    
-    items = sorted(size_dict.values())
+    dictionary = Counter(tangerine)
+    items = sorted(dictionary.values())
 
     while k > 0:
         idx = bisect_left(items, k)
