aadd27fced82a97d997dec521c7ca4955a9f8bdd
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-04-16 11:19:49 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.16 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/2/70129. 이진 변환 반복하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 1.42 ms
+메모리: 10.3 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 16일 11:15:49
+2024년 04월 16일 11:19:47
 
 ### 문제 설명
 
--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -3,18 +3,11 @@ def solution(s):
     cnt0 = 0
     turn = 0
     
-    while True:
+    while s != "1":
         turn += 1
-        cnt0 += s.count("0")
-        s = s.replace("0", "")
-        length = len(s)
-
-        s = bin(int(length))
-        if s == bin(1):
-            answer = [turn, cnt0]
-            break
-        else:
-            s = s[2:]
+        num = s.count("1")
+        cnt0 += len(s) - num
+        s = bin(num)[2:]
             
 
-    return answer
+    return [turn, cnt0]
