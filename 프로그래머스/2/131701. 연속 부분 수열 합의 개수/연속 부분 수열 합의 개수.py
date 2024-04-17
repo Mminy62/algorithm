6a7351531bdffc60bb2762fbc8ac3806834f3069
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-04-17 12:32:09 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 272.92 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,13 +1,21 @@
 def solution(elements):
-    answer = 0
-    origin = elements
-    telements = elements + elements
+#     origin = elements
+#     telements = elements + elements
     
-    n = len(origin)
-    temp = set()
-    for size in range(1, n + 1):
-        for s in range(n):
-            # 시작부터 size까지
-            temp.add(sum(telements[s: s + size]))
+#     n = len(origin)
+#     temp = set()
+#     for size in range(1, n + 1):
+#         for s in range(n):
+#             # 시작부터 size까지
+#             temp.add(sum(telements[s: s + size]))
+
+    n = len(elements)
+    result = set()
+    for i in range(n):
+        ssum = elements[i]
+        result.add(ssum)
+        for j in range(i + 1, i + n):
+            ssum += elements[j % n]
+            result.add(ssum)
     
-    return len(temp)
+    return len(result)
