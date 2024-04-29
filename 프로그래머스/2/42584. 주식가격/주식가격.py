73a8fedd06fa9ed7b4e6eee75fc8dd0a3c2f07fa
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-04-29 13:22:57 +0900

    [level 2] Title: 주식가격, Time: 20.99 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,24 +1,12 @@
 def solution(prices):
-    answer = []
-    n = len(prices)
-    for i in range(1, n + 1):
-        cur = prices[i - 1]
-        ans = 0
-        for j in range(i, n + 1):
-            if prices[j - 1] < cur:
-                ans = j - i
-                break
-        if not ans:
-            ans = n - i
-        
-        answer.append(ans)
-                
-                
-        
-    
+    stack = []
+    answer = [0] * len(prices)
+    for i in range(len(prices)):
+        if stack != []:
+            while stack != [] and stack[-1][1] > prices[i]:
+                past, _ = stack.pop()
+                answer[past] = i - past
+        stack.append([i, prices[i]])
+    for i, s in stack:
+        answer[i] = len(prices) - 1 - i
     return answer
-
-'''
-끝에서부터 기준으로, 최소값을 저장한 배열
-'''
-
