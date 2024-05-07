df4d0bd7e95579fb989bfeb648a50076192060bc
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-05-07 10:52:54 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.py
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.py
@@ -1,38 +1,25 @@
 def solution(h1, m1, s1, h2, m2, s2):
     answer = 0
-    # 시작시간과 끝시간을 초단위로 변환
-    startTime = h1 * 3600 + m1 * 60 + s1
-    endTime = h2 * 3600 + m2 * 60 + s2
     
-    spre = s1 * 360 / 60
-    mpre = m1 * 360 / 60 + s1 * 360 / 60 / 60
-    hpre = h1 * 360 / 12 + m1 * 360 / 12 / 60 + s1 * 360 / 12 / 60 / 60 
+    # 초침은 60으로 나눠지면 1초 지난 것, 1초에 1도씩 움직임
+    # 
+    # 시간을 전부 초로 바꿔서
+    # 그 초의 움직임동안 마주치는 횟수
+    time = (h2 * 3600 + m2 * 60 + s2) - (h1 * 3600 + m1 * 60 + s1)
+    sunit = s1 * 360 / 60
+    munit = m1 * 360 / 60 + s1 * 360 / 60 / 60
+    hunit = h1 * 360 / 12 + m1 * 360 / 12 / 60 + s1 * 360 / 12 / 60 / 60 
     
-    if startTime == 0 * 3600 or startTime == 12 * 3600:
-        answer += 1
+    # spre가 
+    print(spre, mpre, hpre, spre + sunit, mpre + munit, hunit)
+    for _ in range(time):
         
-            
-    for time in range(startTime, endTime): # 처음시간 체크
-        hpre = time / 120 % 360
-        mpre = time / 10 % 360
-        spre = time * 6 % 360
-        
-        # 다음 위치가 360도가 아닌 0도로 계산되어 카운팅에 포함되지 않는 경우 방지
-        # 이동했을 때 지나쳤거나 같아졌는지를 비교하는 것이므로 현재위치는 해줄 필요없음
-        hpost = 360 if (time + 1) / 120 % 360 == 0 else (time + 1) / 120 % 360
-        mpost = 360 if (time + 1) / 10 % 360 == 0 else (time + 1) / 10 % 360
-        spost = 360 if (time + 1) * 6 % 360 == 0 else (time + 1) * 6 % 360
-
-        if (spre < mpre and mpost <= spost):
-            answer += 1
-
-        if (spre < hpre and hpost <= spost):
-            answer += 1
-        
-        if mpost == hpost and spost == mpost:
-            answer -= 1
-        
-        spre, mpre, hpre = spost, mpost, hpost
-
 
     return answer
+'''
+
+mAngle = m * 360 / 60 + s * 360 / 60 / 60 입니다.
+시침은 분침의 현재 위치에 따라 360 / 12 안에서 위치가 결정 되고, 거기에 초침의 위치에 따라 360/12 / 60 안에서 위치가 결정 됩니다.
+hAngle = h * 360 / 12 + m * 360 / 12 / 60 + s * 360 / 12 / 60 / 60 
+
+'''
