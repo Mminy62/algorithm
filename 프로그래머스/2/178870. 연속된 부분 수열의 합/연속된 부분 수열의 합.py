e896b7bed95898e88bb0de00a4a1900a4069075e
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-05-17 15:19:35 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,32 +1,25 @@
-'''
-임의의 두 인덱스의 원소, 그 사이 원소를 모두 포함하는 부분 수열
-부분 수열의 합은 k
-합이 k인 부분 수열이 여러개인 경우 길이가 짧은 수열을 찾는다 
-길이가 짧은게 여러개면, 시작인덱스가 작은걸로 찾기 
-시작, 마지막 인덱스를 담은 배열을 return 
-'''
-
+# 누적합 풀이
 def solution(sequence, k):
+    answer = [0, 10 ** 6]
+    n = len(sequence)
+    prefix = [0] * (n)
+    tmp = 0
+    for i in range(1, n):
+        tmp += sequence[i]
+        prefix[i] = tmp
+
     start = 0
     end = 0
-    n = len(sequence)
-    tmp = sequence[start]
-    answer = [0, n + 1]
-    
+    tmp = 0
     while end < n and start <= end:
-        if tmp == k and answer[1] - answer[0] > end - start:
-            answer = [start, end]
-            tmp -= sequence[start]
+        tmp = prefix[end] - prefix[start] + sequence[start]
+        if tmp > k:
             start += 1
-        
-        elif tmp > k:
-            tmp -= sequence[start]
-            start += 1
-        else: # tmp < k
+        elif tmp < k:
             end += 1
-            if end > n - 1:
-                break
-            tmp += sequence[end]
-            
-        
+        else:
+            if answer[1] - answer[0] > end - start:
+                answer = [start, end]
+            end += 1
+                
     return answer
