3283d386087c61276580b10b95a77ca3e8fa1f2b
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-10-02 17:31:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 173.24 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,19 +1,8 @@
 from collections import Counter
 
 def solution(participant, completion):
-    answer = ''
-    set_part = set(participant)
-    set_com = set(completion)
+    count_part = Counter(participant)
+    count_com = Counter(completion)
+    answer = count_part - count_com
     
-    result = set_part - set_com
-    if result:
-        answer = result.pop()
-    
-    else: # 동명이인이 답인 경우
-        count_part = Counter(participant)
-        count_com = Counter(completion)
-        for part, com in zip(sorted(count_part.items()), sorted(count_com.items())):
-            if part[1] != com[1]:
-                answer = part[0]
-    
-    return answer
+    return list(answer.keys())[0]
