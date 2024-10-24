62089701553bf78f03dfeb6e8d6dee7d08edfd09
Minyoung Lee <66752398+Mminy62@users.noreply.github.com>
2024-10-24 17:03:05 +0900

    [Silver I] Title: 단지번호붙이기, Time: 88 ms, Memory: 109404 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,39 +1,36 @@
 N = int(input())
 board = []
 for _ in range(N):
-    temp = list(input())
-    board.append(list(map(int, temp)))
+    board.append(list(map(int, list(input()))))
 
 visited = [[False] * N for _ in range(N)]
-
-# 방문했는지는 visited를 global로
-
-# dfs
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-def dfs(x, y, node):
-    for k in range(4):
-        nx = x + dx[k]
-        ny = y + dy[k]
-
-        if nx < 0 or nx >= N or ny < 0 or ny >= N:
-            continue
-        if board[nx][ny] and not visited[nx][ny]:
-            visited[nx][ny] = True
-            node = dfs(nx, ny, node + 1) # 깊게 들어간 후 막힌 곳에 대한 Node를 가져오는 것. 가져온 노드는 또 그 값에 더해서 계속 더함
-
-    return node
-
 cnt = 0
-result = []
+def dfs(x, y):
+    global cnt
+    if x < 0 or x >= N or y < 0 or y >= N:
+        return
+
+    if board[x][y] == 1 and not visited[x][y]:
+        visited[x][y] = True
+        cnt += 1
+        dfs(x - 1, y)
+        dfs(x + 1, y)
+        dfs(x, y - 1)
+        dfs(x, y + 1)
+
+    return
+
+group = 0
+arr = []
 for i in range(N):
     for j in range(N):
-        if board[i][j] and not visited[i][j]:
-            cnt += 1
-            visited[i][j] = True
-            result.append(dfs(i, j, 1))
+        if board[i][j] == 1 and not visited[i][j]:
+            group += 1
+            dfs(i, j)
+            arr.append(cnt)
+            cnt = 0
 
-print(cnt)
-result.sort()
-for item in result:
-    print(item)
+print(group)
+arr.sort()
+for num in arr:
+    print(num)
