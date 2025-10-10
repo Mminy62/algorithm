78fbf3c045005466bb053ef6d937b599878d8e1e
이민영 <66752398+Mminy62@users.noreply.github.com>
2025-10-11 01:39:50 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 354.68 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -0,0 +1,20 @@
+def solution(cap, n, deliveries, pickups):
+    answer = 0
+    d = 0
+    p = 0
+    pos = n - 1
+
+    for i in range(n - 1, -1, -1):
+        d += deliveries[i]
+        p += pickups[i]
+
+        while d > cap or p > cap:
+            d -= cap
+            p -= cap
+            answer += 2 * (pos + 1)
+            pos = i
+
+    if d > 0 or p > 0:
+        answer += 2 * (pos + 1)
+
+    return answer
